7ab96482947edc17587601a04f86f0d4bdafa9f9
rwaals <rwaals>
2008-12-02 21:10:06 +0000

    *** empty log message ***

--- _source/net/datacrow/reporting/transformers/XmlTransformers.java
+++ _source/net/datacrow/reporting/transformers/XmlTransformers.java
@@ -39,6 +39,7 @@ public abstract class XmlTransformers {
     static {
         transformers.add(new Xml2HtmlTransformer());
         transformers.add(new Xml2PdfTransformer());
+        transformers.add(new Xml2RtfTransformer());
     }
     
     public static Collection<XmlTransformer> getTransformers() {
