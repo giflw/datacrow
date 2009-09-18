79580e60d1aaf9b1fc7f0609c933acebad3dca29
rwaals <rwaals>
2009-09-18 20:13:48 +0000

    *** empty log message ***

--- _source/net/datacrow/console/windows/FieldDefinitionsDialog.java
+++ _source/net/datacrow/console/windows/FieldDefinitionsDialog.java
@@ -187,7 +187,6 @@ public class FieldDefinitionsDialog extends DcDialog implements ActionListener {
         public DefinitionPanel(DcModule module) {
             this.module = module;
             table = ComponentFactory.getDCTable(false, false);
-            table.setShowVerticalLines(true);
             buildPanel();
         }
 
