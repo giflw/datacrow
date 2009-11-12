eb5e2dfd963fe429c012a2b7e274787321e7fe46
rwaals <rwaals>
2009-11-12 15:02:51 +0000

    *** empty log message ***

--- _source/net/datacrow/core/objects/DcMapping.java
+++ _source/net/datacrow/core/objects/DcMapping.java
@@ -108,14 +108,25 @@ public class DcMapping extends DcObject {
      * The object ID of the parent.
      */
     public String getParentId() {
-        return (String) getValue(_A_PARENT_ID);
+        return getValueDef(_A_PARENT_ID).getValueAsString();
     }
 
     /**
      * The object ID of the referenced item.
      */
     public String getReferencedId() {
-        return (String) getValue(_B_REFERENCED_ID);
+        String id = getValueDef(_B_REFERENCED_ID).getValueAsString();
+        
+        try {
+            Long.parseLong(id);
+        } catch (Exception e) {
+            logger.warn("Invalid ID for reference in mapping to item " + referencedObj + ", module " + getModule(), e);
+            id = referencedObj.getID();
+            logger.warn("Invalid ID corrected to " + id);
+            setValue(_B_REFERENCED_ID, id);
+        }
+        
+        return id;
     }
     
     @Override
