f5b480232d40185d608d3389979280ae1c1638fb
rwaals <rwaals>
2011-11-19 13:53:19 +0000

    *** empty log message ***

--- _source/net/datacrow/core/DataCrow.java
+++ _source/net/datacrow/core/DataCrow.java
@@ -97,7 +97,7 @@ public class DataCrow {
     private static SplashScreen splashScreen;
     
     private static Platform platform = new Platform();
-    private static Version version = new Version(3, 9, 9, 0);
+    private static Version version = new Version(3, 9, 10, 0);
     
     public static String installationDir;
     public static String imageDir;
