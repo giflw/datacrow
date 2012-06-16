505d14f65e3cd720c837f42782dd0b9b05aec38e
rwaals <rwaals>
2012-06-16 08:58:41 +0000

    *** empty log message ***

--- installer/compile_installer.bat
+++ installer/compile_installer.bat
@@ -3,6 +3,6 @@
 @rem DOS Batch file to invoke the compiler
 @rem =====================================
 
-call "C:/Users/RJ/Documents/Development/tools/izpack/bin/compile.bat" installer.xml -b ../
-"C:/Users/RJ/Documents/Development/tools/jsmooth/jsmooth/jsmoothcmd" ./installer.jsmooth
+call "C:/Users/RJ/Data/Development/tools/izpack/bin/compile.bat" installer.xml -b ../
+"C:/Users/RJ/Data/Development/tools/jsmooth/jsmooth/jsmoothcmd" ./installer.jsmooth
 @echo on
