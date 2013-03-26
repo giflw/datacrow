236066f402749d5aa8097e32c7f22855d65bf8e2
rwaals <rwaals>
2013-03-26 22:39:53 +0000

    *** empty log message ***

--- _source/net/datacrow/util/HtmlUtils.java
+++ _source/net/datacrow/util/HtmlUtils.java
@@ -75,9 +75,9 @@ public class HtmlUtils {
         
         ByteArrayInputStream in = new ByteArrayInputStream(html.getBytes());
         
-        if (logger.isDebugEnabled()) {
-            Utilities.writeToFile(html.getBytes(), "c:\\online_service_document.xml");
-        }
+//        if (logger.isDebugEnabled()) {
+//            Utilities.writeToFile(html.getBytes(), "c:\\online_service_document.xml");
+//        }
         
         Reader reader = new InputStreamReader(in);
         Document document = builder.newDocument();
