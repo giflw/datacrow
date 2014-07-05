a377ce84fb645f834dfe755bb0d692d5c65c39cf
rwaals <rwaals>
2014-07-05 19:49:20 +0000

    *** empty log message ***

--- hsqlmanager.cmd
+++ hsqlmanager.cmd
@@ -1,1 +1,1 @@
-java -Xmx512m -classpath ./lib/hsqldb.jar org.hsqldb.util.DatabaseManager %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Xmx512m -classpath ../datacrow-server/lib/hsqldb/hsqldb.jar org.hsqldb.util.DatabaseManager %1 %2 %3 %4 %5 %6 %7 %8 %9
